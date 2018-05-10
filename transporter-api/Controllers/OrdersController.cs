551b29f89796fc628994195ce03a652a605dff69
Rustem Mazitov <rmazitov@d-l-s.ru>
2018-05-10 10:56:13 +0300

    Add orders controller

---
+++ transporter-api/Controllers/OrdersController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using transporter_api.WebSockets;
+
+namespace transporter_api.Controllers
+{
+    [Route("orders")]
+    public class OrdersController : Controller
+    {
+        // GET api/orders
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/orders/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/orders
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Order newOrder)
+        {
+            await MobileSocket.SendToAllMobileSockets(new OrderAvailablePayload
+            {
+                Payload = newOrder
+            });
+            return Ok();
+        }
+
+        // PUT api/orders/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/orders/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
