9a3e1025b55b4b485d8e75afb0e3c4e443897bfb
Rustem Mazitov <rmazitov@d-l-s.ru>
2018-06-03 20:51:41 +0300

    Try allow cors

--- transporter-api/Startup.cs
+++ transporter-api/Startup.cs
@@ -31,6 +31,7 @@ namespace transporter_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().AddJsonOptions(options =>
             {
                 options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
